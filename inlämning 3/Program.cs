bc20f6c4ecca434380cbc2971652d64fca529a27
muja07001 <muja07001@E-PW05CGR9>
2024-09-12 14:51:44 +0200

    Add project files.

---
+++ inlämning 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inlämning_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+
+
+            
+        }
+    }
+}
